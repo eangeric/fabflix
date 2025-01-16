eb13d8b92591c9be5ba5966e118dd45c41e6183a
Eric Eang <eric.eang@yahoo.com>
2025-01-16 14:43:21 -0800

    Add N/A displayed for null birthdays

--- WebContent/pages/single-star/single-star.js
+++ WebContent/pages/single-star/single-star.js
@@ -44,9 +44,11 @@ function handleResult(resultData) {
     // find the empty h3 body by id "star_info"
     let starInfoElement = jQuery("#star_info");
 
+    // If birthday is null set to N/A, else use the birthday
+    const birthDay = resultData[0]["star_dob"] === null ? "N/A" : resultData[0]["star_dob"]
     // append two html <p> created to the h3 body, which will refresh the page
     starInfoElement.append("<p>Star Name: " + resultData[0]["star_name"] + "</p>" +
-        "<p>Date Of Birth: " + resultData[0]["star_dob"] + "</p>");
+        "<p>Date Of Birth: " + birthDay + "</p>");
 
     console.log("handleResult: populating movie table from resultData");
 
