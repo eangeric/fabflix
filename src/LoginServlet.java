b7221f449d06eca0f043f2f215f027eb1067cf27
Eric Eang <eange@uci.edu>
2025-01-21 12:02:11 -0800

    Initial setup for project2

---
+++ src/LoginServlet.java
@@ -0,0 +1,11 @@
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
+public class LoginServlet extends HttpServlet {
+
+}
