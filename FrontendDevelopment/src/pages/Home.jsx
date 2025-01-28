9508ac7742e332682adf87894ccd875e24d75ec7
Eric Eang <eange@uci.edu>
2025-01-27 18:35:58 -0800

    Add static image routing.

--- FrontendDevelopment/src/pages/Home.jsx
+++ FrontendDevelopment/src/pages/Home.jsx
@@ -5,30 +5,28 @@ import { MovieTable } from "../components/MovieTable.jsx";
 import { useFetch } from "../hooks/useFetch";
 
 export default function Home() {
+  // --- BEGIN: Movie Table from Stage 1 ----
+  const { data, loading, error } = useFetch("/fabflix/api/movies");
+  const movieData = data || null;
+  //console.log(data);
+  return (
+    <div className="text-white">
+      {/* If data is still loading */}
+      {loading && <h1>Loading movie data...</h1>}
 
-    // --- BEGIN: Movie Table from Stage 1 ----
-    const { data, loading, error } = useFetch(
-        '/fabflix/api/movies');
-    const movieData = data || null;
-    //console.log(data);
-    return (
-        <div className = "text-white">
-            {/* If data is still loading */}
-            {loading && <h1>Loading movie data...</h1>}
+      {/* If error */}
+      {error && <h1>Error: {error}</h1>}
 
-            {/* If error */}
-            {error && <h1>Error: {error}</h1>}
+      {/* If movieData loaded display information */}
+      {movieData && (
+        <>
+          <MovieTable movieData={movieData} />
+        </>
+      )}
 
-            {/* If movieData loaded display information */}
-            {movieData && (
-                <>
-                    <MovieTable movieData={movieData}/>
-                </>
-            )}
-
-            {/* Fallback for unexpected states */}
-            {!loading && !error && !movieData && <h1>No movie data available</h1>}
-        </div>
-    );
-    // --- END: Movie Table from Stage 1 ----
+      {/* Fallback for unexpected states */}
+      {!loading && !error && !movieData && <h1>No movie data available</h1>}
+    </div>
+  );
+  // --- END: Movie Table from Stage 1 ----
 }
