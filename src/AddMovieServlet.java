56b10d9637b8547ad2250eec80d2593f1558b314
Eric Eang <eange@uci.edu>
2025-02-16 23:54:11 -0800

    Add APIs

---
+++ src/AddMovieServlet.java
@@ -0,0 +1,98 @@
+import com.google.gson.JsonObject;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+@WebServlet(name = "AddMovieServlet", urlPatterns = "/api/add-movie")
+public class AddMovieServlet extends HttpServlet {
+  // Create a dataSource which registered in web.
+  private DataSource dataSource;
+
+  public void init(ServletConfig config) {
+    try {
+      dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+    } catch (NamingException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String title = request.getParameter("title");
+    String year = request.getParameter("year");
+    String director = request.getParameter("director");
+    String starName = request.getParameter("starName");
+    String starYear = request.getParameter("starYear");
+    String genre = request.getParameter("genre");
+
+    response.setContentType("application/json"); // Set content type
+    JsonObject responseJsonObject = new JsonObject(); // JSON object for response
+
+    // Validate inputs
+    if (title == null || year == null || director == null || starName == null || genre == null) {
+      responseJsonObject.addProperty("status", "failed");
+      responseJsonObject.addProperty("message", "Missing request parameters");
+      response.getWriter().write(responseJsonObject.toString());
+      return;
+    }
+
+    try (Connection conn = dataSource.getConnection()) {
+      String procedure = "{CALL add_movie(?, ?, ?, ?, ?, ?)}";
+      PreparedStatement statement = conn.prepareStatement(procedure);
+
+      statement.setString(1, title);
+      statement.setInt(2, Integer.parseInt(year));
+      statement.setString(3, director);
+      statement.setString(4, starName);
+
+      if (starYear == null || starYear.trim().isEmpty()) {
+        statement.setNull(5, java.sql.Types.INTEGER);
+      } else {
+        statement.setInt(5, Integer.parseInt(starYear));
+      }
+
+      statement.setString(6, genre);
+      ResultSet rs = statement.executeQuery();
+
+      if (rs.next()) {
+        String resultMessage = rs.getString("message");
+
+        if (resultMessage.startsWith("ERROR:")) {
+          responseJsonObject.addProperty("status", "failed");
+          responseJsonObject.addProperty("message", "Movie '" + title + "' already exists.");
+        } else {
+          responseJsonObject.addProperty("status", "success");
+          responseJsonObject.addProperty("message", "Movie '" + title + "' added successfully.");
+        }
+      }
+
+    } catch (Exception e) {
+      // Write error message JSON object to output
+      responseJsonObject = new JsonObject();
+      responseJsonObject.addProperty("status", "error");
+      responseJsonObject.addProperty("errorMessage", e.getMessage());
+      // Log error to localhost log
+      request.getServletContext().log("Error:", e);
+
+    }
+
+    // Write to response
+    response.getWriter().write(responseJsonObject.toString());
+
+  }
+}
---
+++ src/EmployeeServlet.java
@@ -0,0 +1,107 @@
+import com.google.gson.JsonObject;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jasypt.util.password.StrongPasswordEncryptor;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+@WebServlet(name = "EmployeeServlet", urlPatterns = "/api/employee")
+public class EmployeeServlet extends HttpServlet {
+  // Create a dataSource which registered in web.
+  private DataSource dataSource;
+
+  public void init(ServletConfig config) {
+    try {
+      dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+    } catch (NamingException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String email = request.getParameter("email");
+    String password = request.getParameter("password");
+
+    response.setContentType("application/json"); // Set content type
+    JsonObject responseJsonObject = new JsonObject(); // JSON object for response
+
+    // Validate inputs
+    if (email == null || password == null || email.isEmpty() || password.isEmpty()) {
+      responseJsonObject.addProperty("status", "failed");
+      responseJsonObject.addProperty("message", "Missing email or password");
+      response.getWriter().write(responseJsonObject.toString());
+      return;
+    }
+
+    // Get a connection from dataSource and let resource manager close the
+    // connection after usage.
+    try (Connection conn = dataSource.getConnection()) {
+      // Work on connecting to db
+      // Create query string for movies
+
+      String query = "SELECT * FROM employees WHERE email = ? ";
+
+      // Prepare the statement
+      PreparedStatement statement = conn.prepareStatement(query);
+      // Set email parameter
+      statement.setString(1, email);
+
+      // Execute the query
+      ResultSet rs = statement.executeQuery();
+
+      boolean success = false;
+      if (rs.next()) {
+        // get the encrypted password from the database
+        String encryptedPassword = rs.getString("password");
+
+        // use the same encryptor to compare the user input password with encrypted
+        // password stored in DB
+        success = new StrongPasswordEncryptor().checkPassword(password, encryptedPassword);
+      }
+
+      if (success) {
+        // Login success:
+        // set this user into the session
+        // request.getSession().setAttribute("user", new User(email));
+        // System.out.println("User added to session: " + email);
+        // Write to json object
+        responseJsonObject.addProperty("status", "success");
+        responseJsonObject.addProperty("message", "Logged in");
+      } else {
+        // Login failed:
+        // Log to localhost log
+        request.getServletContext().log("Login failed");
+        // Write to json object
+        responseJsonObject.addProperty("status", "failed");
+        responseJsonObject.addProperty("message", "Invalid email or password");
+      }
+
+    } catch (Exception e) {
+      // Write error message JSON object to output
+      responseJsonObject = new JsonObject();
+      responseJsonObject.addProperty("errorMessage", e.getMessage());
+      // Log error to localhost log
+      request.getServletContext().log("Error:", e);
+
+    }
+
+    // Write to response
+    response.getWriter().write(responseJsonObject.toString());
+
+  }
+}
