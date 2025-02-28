afe29d2b56782fa1e26c658fd402e2b6fccfa25a
Eric Eang <eange@uci.edu>
2025-02-27 16:23:45 -0800

    Full Text API

--- FrontendDevelopment/src/components/Navbar.jsx
+++ FrontendDevelopment/src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+  const data = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
+
   return (
     <nav className="bg-fabflix-primary fixed w-full z-20 top-0 start-0">
-      <div className="max-w-screen-xl-2 mx-auto flex items-center justify-between px-4 md:px-6 relative">
+      <div className="max-w-screen-xl-2 mx-auto flex items-center justify-between px-4 relative">
         {/* Logo Left Section */}
         <Link to="/" className="flex items-center space-x-2">
           <img
@@ -15,8 +17,35 @@ export const Navbar = () => {
           <h1 className="text-4xl font-bold text-white">Fabflix</h1>
         </Link>
 
+        {/* Middle */}
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input w-[15rem] bg-white text-black"
+          />
+          <div className="hidden absolute w-full max-h-50 overflow-y-scroll bg-white text-black rounded-md divide-y-2">
+            {data.map((d, i) => {
+              return (
+                <div key={i} className="flex items-center h-10">
+                  {d}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
         {/* Right Section */}
-        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <div className="flex gap-4">
+          <Link
+            to="/"
+            className="flex items-center px-4 hover:text-blue-400
+                text-white
+                border-gray-700
+                transition duration-300 ease-in-out"
+          >
+            Browse
+          </Link>
           <Link
             to="/search"
             className="flex items-center space-x-3 rtl:space-x-reverse"
@@ -37,29 +66,6 @@ export const Navbar = () => {
               style={{ width: "40px" }}
             />
           </Link>
-        </div>
-
-        {/* Centered Browse Section */}
-        <div
-          className="absolute left-1/2 transform -translate-x-1/2 md:static md:transform-none"
-          id="navbar-sticky"
-        >
-          <ul
-            className="flex flex-col p-4  font-medium
-            border-gray-700 items-center"
-          >
-            <li>
-              <Link
-                to="/"
-                className="block py-2 px-3 md:p-0 hover:text-blue-500
-                text-white
-                border-gray-700
-                transition duration-300 ease-in-out"
-              >
-                Browse
-              </Link>
-            </li>
-          </ul>
         </div>
       </div>
     </nav>
---
+++ src/FullTextServlet.java
@@ -0,0 +1,113 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+// Declaring a WebServlet called SearchServlet, which maps to url "/api/search"
+@WebServlet(name = "FullTextServlet", urlPatterns = "/api/fulltext")
+public class FullTextServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  // Create a dataSource which registered in web.
+  private DataSource dataSource;
+
+  public void init(ServletConfig config) {
+    try {
+      dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+    } catch (NamingException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json"); // Response mime type
+    PrintWriter out = response.getWriter();
+
+    // Get a connection from dataSource and let resource manager close the
+    // connection after usage.
+    try (Connection conn = dataSource.getConnection()) {
+      // Create query string for movies
+      StringBuilder queryBuilder = new StringBuilder(
+          "SELECT m.id, m.title, m.year " +
+              "FROM movies m " +
+              "LEFT JOIN ratings r ON m.id = r.movieId " +
+              "WHERE MATCH(m.title) AGAINST(? IN BOOLEAN MODE)");
+
+      String requestedSearch = request.getParameter("search");
+
+      // Convert query to string
+      String query = queryBuilder.toString();
+      // Create statement to execute
+      PreparedStatement statement = conn.prepareStatement(query);
+
+      // If search parameter is provided, format it properly
+      if (requestedSearch != null && !requestedSearch.trim().isEmpty()) {
+        // Convert query into full-text format by adding `*` to each keyword
+        String[] tokens = requestedSearch.split(" ");
+
+        for (int i = 0; i < tokens.length; i++) {
+          tokens[i] = "+" + tokens[i] + "*";
+        }
+
+        String formattedSearch = String.join(" ", tokens);
+        System.out.println(formattedSearch);
+
+        statement.setString(1, formattedSearch);
+      } else {
+        statement.setString(1, "");
+      }
+
+      // Get results
+      ResultSet rs = statement.executeQuery();
+
+      // json array to hold our json object
+      JsonArray jsonArray = new JsonArray();
+
+      // Iterate through each row of rs
+      while (rs.next()) {
+        // Get from query results for movies
+        String moviesId = rs.getString("m.id");
+        String movieTitle = rs.getString("m.title");
+        String movieYear = rs.getString("m.year");
+        // Create a JsonObject based on the data we retrieve from rs
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("movies_id", moviesId);
+        jsonObject.addProperty("movie_title", movieTitle);
+        jsonObject.addProperty("movie_year", movieYear);
+
+        jsonArray.add(jsonObject);
+      }
+
+      // Write to response
+      out.write(jsonArray.toString());
+
+    } catch (Exception e) {
+
+      // Write error message JSON object to output
+      request.getServletContext().log("Error: ", e);
+      JsonObject errorResponse = new JsonObject();
+      errorResponse.addProperty("errorMessage", e.getMessage());
+      out.write(errorResponse.toString());
+
+      response.setStatus(500);
+    } finally {
+      out.close();
+    }
+
+  }
+}
